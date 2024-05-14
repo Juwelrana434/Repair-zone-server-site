3dbc188d6ec7c3af4388abdb41a7a44648aecbe5
Juwelrana434 <jewel.rana434@gmail.com>
2024-05-14 17:49:58 +0600

    Theme implement

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function run() {
     // use server data/ read server
     const servicesCollection = client.db('repair-zone').collection('services');
     const bookServicesCollection = client.db('repair-zone').collection('bookServices');
-    const addServicesCollection = client.db('repair-zone').collection('addServices');
+    const commentCollection = client.db('repair-zone').collection('comment');
    
     //data read form server
     app.get('/services', async(req, res) => {
@@ -89,6 +89,22 @@ async function run() {
       
       })
       
+    // add comment  data send to server
+    
+    app.post('/comment', async(req, res) => {
+      const comment  = req.body;
+      console.log(comment);
+      const result = await commentCollection.insertOne(comment );
+      res.send(result)
+      
+      })
+      //comment data read form server
+    app.get('/comment', async(req, res) => {
+    const cursor = commentCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+    });
+      
     
     //  booking information send request to server
     
@@ -100,7 +116,7 @@ async function run() {
     
     })
     
-    // booking data read from server
+    // booking data read by own email from server
     app.get('/booking', async(req, res) => {
     let query = {};
     if(req.query.email){
@@ -110,17 +126,64 @@ async function run() {
     res.send(result);
     });
     
+    //all booking data read form server
+    app.get('/booking', async(req, res) => {
+      const cursor = bookServicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      });
     
     // for my add service data read 
-    app.get('/addService/:email', async (req, res) => {
+    app.get('/addService/email/:email', async (req, res) => {
       // console.log(req.params.email);
-        const cursor = touristCollection.find({email:req.params.email});
+        const cursor = servicesCollection.find({email:req.params.email});
         const result = await cursor.toArray();
         res.send(result);
         })
+        
+         // data update 
+    
+    app.get('/services/:id', async (req, res) => {
+      console.log(req.params.id);
+      const result = await servicesCollection.findOne({_id: new ObjectId(req.params.id)})
+      res.send(result);
+      
+      })
+    
+    
+    app.put('/services/:id', async (req, res) => {
+    const id  = req.params.id;
+    console.log(id);
+    const filter = {_id: new ObjectId(id)}
+    const options = {upsert: true}
+    const updateService = req.body;
+    
+    const servicesUpdate = {
+    $set: {
+      service_image: updateService.service_image,
+      service_name: updateService.service_name,
+      location: updateService.location,
+      service_description: updateService.service_description,
+      service_price: updateService.service_price,
+      
+    }}
+    const result = await servicesCollection.updateOne(filter, servicesUpdate, options);
+    res.send(result);
+    
+    })
+    
+        // data delete method
+    
+    app.delete('/addServices/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result);
+      
+      })
     
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
